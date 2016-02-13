f642b9e56d839589c300a5339d1d5d4a95b1b610
frankNowinski <nowinski.frank@gmail.com>
2016-02-12 19:25:51 -0500

    finish add_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,21 @@
 class Cart < ActiveRecord::Base
-  belongs_to :line_item
+  has_many :line_items
+  has_many :items, through: :line_items
   belongs_to :user
   belongs_to :order
+
+  def total
+    line_items.collect{ |line_item| line_item.item.price }.inject(:+)
+  end
+
+  def add_item(item_id)
+    new_item = self.line_items.build(item_id: item_id)
+    if new_item.quantity.nil?
+      new_item.quantity = 1
+    else
+      new_item.quantity += 1
+    end
+    new_item
+  end
+
 end
